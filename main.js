2dd16f18db45bb51fbd6c93ff71c74cebc3c046e
zk-phi <zk-phi@users.noreply.github.com>
2022-03-09 13:48:51 +0900

    Indentation width changes

--- main.js
+++ main.js
@@ -1,122 +1,122 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-        preset: "",
-        param: {
-            handNum: 8,
-            deckNum: 60,
-            targets: [4],
-            targetLabels: [""],
-            searches: [],
-            searchLabels: []
-        },
-        result: {
-            hands: [],
-            successRate: "-"
-        }
+  el: "#app",
+  data: {
+    preset: "",
+    param: {
+      handNum: 8,
+      deckNum: 60,
+      targets: [4],
+      targetLabels: [""],
+      searches: [],
+      searchLabels: []
     },
-    watch: {
-        param: {
-            handler: function () {
-                vm.result.hands = [];
-                vm.result.successRate = "-";
-            },
-            deep: true
-        }
-    },
-    filters: {
-        formatProb: function (prob) {
-            const match = (prob + "").match(/^[^1-9]*\.00+[^0]/);
-            if (match) return match[0];
-
-            const parts = (prob + "").split(".");
-            return parts[0] + "." + (parts[1] || "").substring(0, 2);
-        }
-    },
-    methods: {
-        delTarget: function (ix) {
-            vm.param.targets.splice(ix, 1);
-            vm.param.targetLabels.splice(ix, 1);
-        },
-        addTarget: function () {
-            vm.param.targets.push(4);
-            vm.param.targetLabels.push("");
-        },
-        delSearch: function (ix) {
-            vm.param.searches.splice(ix, 1);
-            vm.param.searchLabels.splice(ix, 1);
-        },
-        add100Search: function () {
-            vm.param.searches.push({ type: 'prob', prob: 100, target: 0, num: 4 });
-            vm.param.searchLabels.push("");
-        },
-        addProbSearch: function () {
-            vm.param.searches.push({ type: 'prob', prob: 50, target: 0, num: 4 });
-            vm.param.searchLabels.push("");
-        },
-        addCountSearch: function () {
-            vm.param.searches.push({ type: 'count', count: 7, target: 0, num: 4 });
-            vm.param.searchLabels.push("");
-        },
-        compute: function () {
-            try {
-                vm.result = solve({
-                    deckNum: vm.param.deckNum,
-                    handNum: vm.param.handNum,
-                    targets: vm.param.targets,
-                    searches: vm.param.searches,
-                });
-            } catch {
-                vm.result = { hands: [], successRate: "N/A" };
-            }
-        },
-        changePreset: function () {
-            if (vm.preset == 'tane8') {
-                vm.param.deckNum = 60;
-                vm.param.handNum = 7;
-                vm.param.targets = [8, 1];
-                vm.param.targetLabels = ["種", "デデンネ"];
-                vm.param.searches = [];
-                vm.param.searchLabels = [];
-            } else if (vm.preset == 'support8') {
-                vm.param.deckNum = 60;
-                vm.param.handNum = 8;
-                vm.param.targets = [8];
-                vm.param.targetLabels = ["サポート"];
-                vm.param.searches = [{ type: 'count', count: 7, target: 0, num: 2 }];
-                vm.param.searchLabels = ["ポケギア"];
-            } else if (vm.preset == 'exo') {
-                vm.param.deckNum = 40;
-                vm.param.handNum = 5;
-                vm.param.targets = [1, 1, 1, 1, 1];
-                vm.param.targetLabels = ["左腕", "右腕", "左脚", "右脚", "本体"];
-                vm.param.searches = [];
-                vm.param.searchLabels = [];
-            } else if (vm.preset == 'land') {
-                vm.param.deckNum = 60;
-                vm.param.handNum = 7;
-                vm.param.targets = [20];
-                vm.param.targetLabels = ["土地"];
-                vm.param.searches = [];
-                vm.param.searchLabels = [];
-            } else if (vm.preset == 'evo') {
-                vm.param.deckNum = 60;
-                vm.param.handNum = 8;
-                vm.param.targets = [3];
-                vm.param.targetLabels = ["進化前"];
-                vm.param.searches = [
-                    { type: 'prob', prob: 100, target: 0, num: 4 },
-                    { type: 'prob', prob: 100, target: 0, num: 4 },
-                ];
-                vm.param.searchLabels = ["クイック", "通信"];
-            } else {
-                vm.param.deckNum = 60;
-                vm.param.handNum = 8;
-                vm.param.targets = [4];
-                vm.param.targetLabels = [""];
-                vm.param.searches = [];
-                vm.param.searchLabels = [];
-            }
-        }
+    result: {
+      hands: [],
+      successRate: "-"
     }
+  },
+  watch: {
+    param: {
+      handler: function () {
+        vm.result.hands = [];
+        vm.result.successRate = "-";
+      },
+      deep: true
+    }
+  },
+  filters: {
+    formatProb: function (prob) {
+      const match = (prob + "").match(/^[^1-9]*\.00+[^0]/);
+      if (match) return match[0];
+
+      const parts = (prob + "").split(".");
+      return parts[0] + "." + (parts[1] || "").substring(0, 2);
+    }
+  },
+  methods: {
+    delTarget: function (ix) {
+      vm.param.targets.splice(ix, 1);
+      vm.param.targetLabels.splice(ix, 1);
+    },
+    addTarget: function () {
+      vm.param.targets.push(4);
+      vm.param.targetLabels.push("");
+    },
+    delSearch: function (ix) {
+      vm.param.searches.splice(ix, 1);
+      vm.param.searchLabels.splice(ix, 1);
+    },
+    add100Search: function () {
+      vm.param.searches.push({ type: 'prob', prob: 100, target: 0, num: 4 });
+      vm.param.searchLabels.push("");
+    },
+    addProbSearch: function () {
+      vm.param.searches.push({ type: 'prob', prob: 50, target: 0, num: 4 });
+      vm.param.searchLabels.push("");
+    },
+    addCountSearch: function () {
+      vm.param.searches.push({ type: 'count', count: 7, target: 0, num: 4 });
+      vm.param.searchLabels.push("");
+    },
+    compute: function () {
+      try {
+        vm.result = solve({
+          deckNum: vm.param.deckNum,
+          handNum: vm.param.handNum,
+          targets: vm.param.targets,
+          searches: vm.param.searches,
+        });
+      } catch {
+        vm.result = { hands: [], successRate: "N/A" };
+      }
+    },
+    changePreset: function () {
+      if (vm.preset == 'tane8') {
+        vm.param.deckNum = 60;
+        vm.param.handNum = 7;
+        vm.param.targets = [8, 1];
+        vm.param.targetLabels = ["種", "デデンネ"];
+        vm.param.searches = [];
+        vm.param.searchLabels = [];
+      } else if (vm.preset == 'support8') {
+        vm.param.deckNum = 60;
+        vm.param.handNum = 8;
+        vm.param.targets = [8];
+        vm.param.targetLabels = ["サポート"];
+        vm.param.searches = [{ type: 'count', count: 7, target: 0, num: 2 }];
+        vm.param.searchLabels = ["ポケギア"];
+      } else if (vm.preset == 'exo') {
+        vm.param.deckNum = 40;
+        vm.param.handNum = 5;
+        vm.param.targets = [1, 1, 1, 1, 1];
+        vm.param.targetLabels = ["左腕", "右腕", "左脚", "右脚", "本体"];
+        vm.param.searches = [];
+        vm.param.searchLabels = [];
+      } else if (vm.preset == 'land') {
+        vm.param.deckNum = 60;
+        vm.param.handNum = 7;
+        vm.param.targets = [20];
+        vm.param.targetLabels = ["土地"];
+        vm.param.searches = [];
+        vm.param.searchLabels = [];
+      } else if (vm.preset == 'evo') {
+        vm.param.deckNum = 60;
+        vm.param.handNum = 8;
+        vm.param.targets = [3];
+        vm.param.targetLabels = ["進化前"];
+        vm.param.searches = [
+          { type: 'prob', prob: 100, target: 0, num: 4 },
+          { type: 'prob', prob: 100, target: 0, num: 4 },
+        ];
+        vm.param.searchLabels = ["クイック", "通信"];
+      } else {
+        vm.param.deckNum = 60;
+        vm.param.handNum = 8;
+        vm.param.targets = [4];
+        vm.param.targetLabels = [""];
+        vm.param.searches = [];
+        vm.param.searchLabels = [];
+      }
+    }
+  }
 });
--- solver.js
+++ solver.js
@@ -36,131 +36,131 @@ var searches;
  */
 
 function solve (params) {
-    deckNum = params.deckNum;
-    handNum = params.handNum;
-    targets = params.targets;
-    /* 失敗率に変換しておく */
-    searches = params.searches.map(function (s) {
-        if (s.type == 'prob') {
-            return { failRate: 1.0 - (s.prob / 100.0), num: s.num, target: s.target };
-        } else {
-            /* 枚数固定サーチの失敗率も計算しておく */
-            var a = ncr(params.deckNum - params.handNum - params.targets[s.target], s.count);
-            var b = ncr(params.deckNum - params.handNum, s.count);
-            return { failRate: a / b, num: s.num, target: s.target };
-        }
-    });
-    return solveR({
-        /* 今の手札 */
-        targets: params.targets.map(function () { return 0 }),
-        searches: params.searches.map(function () { return 0 }),
-        other: 0,
-        /* ここまでの場合の数 */
-        cases: 1,
-        /* 現時点の手札で各 target に「触れない」確率 */
-        failRate: params.targets.map(function () { return 1.0 }),
-        /* 今後引けるハンド枚数 */
-        handNum: params.handNum,
-        /* 今後引けるデッキ枚数 (引かないと決めたカードは除く) */
-        deckNum: params.deckNum,
-        /* どこまで見たか */
-        targetsIx: 0,
-        searchesIx: 0
-    });
+  deckNum = params.deckNum;
+  handNum = params.handNum;
+  targets = params.targets;
+  /* 失敗率に変換しておく */
+  searches = params.searches.map(function (s) {
+    if (s.type == 'prob') {
+      return { failRate: 1.0 - (s.prob / 100.0), num: s.num, target: s.target };
+    } else {
+      /* 枚数固定サーチの失敗率も計算しておく */
+      var a = ncr(params.deckNum - params.handNum - params.targets[s.target], s.count);
+      var b = ncr(params.deckNum - params.handNum, s.count);
+      return { failRate: a / b, num: s.num, target: s.target };
+    }
+  });
+  return solveR({
+    /* 今の手札 */
+    targets: params.targets.map(function () { return 0 }),
+    searches: params.searches.map(function () { return 0 }),
+    other: 0,
+    /* ここまでの場合の数 */
+    cases: 1,
+    /* 現時点の手札で各 target に「触れない」確率 */
+    failRate: params.targets.map(function () { return 1.0 }),
+    /* 今後引けるハンド枚数 */
+    handNum: params.handNum,
+    /* 今後引けるデッキ枚数 (引かないと決めたカードは除く) */
+    deckNum: params.deckNum,
+    /* どこまで見たか */
+    targetsIx: 0,
+    searchesIx: 0
+  });
 }
 
 function solveR (state) {
-    if (state.handNum == 0) {
-        /* 手札を引き終わった → 確率計算して返す */
-        var caseProb = state.cases / ncr(deckNum, handNum);
-        var successRate = state.failRate.reduce(function (l, r) { return l * (1.0 - r); }, 1);
-        return {
-            hands: [{
-                targets: state.targets,
-                searches: state.searches,
-                other: state.other,
-                prob: caseProb * 100
-            }],
-            successRate: caseProb * successRate * 100  /* この手札になる確率 x サーチの成功率 */
-        };
-    } else if (state.targetsIx < targets.length) {
-        /* 「欲しいカード」を各何枚素引きするかの場合分け */
-        var hands = [];
-        var successRate = 0;
-        var targetNum = targets[state.targetsIx];
-        for (var i = 0; i <= Math.min(targetNum, state.handNum); i++) {
-            /* targetsIx 番目の「欲しいカード」を i 枚素引きする場合 */
-            var res = solveR({
-                targets: state.targets.map(function (v, ix) {
-                    return v + (ix == state.targetsIx ? i : 0)
-                }),
-                searches: state.searches,
-                other: state.other,
-                cases: state.cases * ncr(targetNum, i),
-                failRate: state.failRate.map(function (v, ix) {
-                    return v * (ix == state.targetsIx ? Math.pow(0, i) : 1)
-                }),
-                handNum: state.handNum - i,
-                deckNum: state.deckNum - targetNum,
-                targetsIx: state.targetsIx + 1,
-                searchesIx: state.searchesIx
-            });
-            hands = hands.concat(res.hands);
-            successRate += res.successRate;
-        }
-        return { hands: hands, successRate: successRate };
-    } else if (state.searchesIx < searches.length) {
-        /* サーチを各何枚引くかの場合分け */
-        var hands = [];
-        var successRate = 0;
-        var search = searches[state.searchesIx];
-        for (var i = 0; i <= Math.min(search.num, state.handNum); i++) {
-            /* searchesIx 番目のサーチを i 枚素引きする場合 */
-            var res = solveR({
-                targets: state.targets,
-                searches: state.searches.map(function (v, ix) {
-                    return v + (ix == state.searchesIx ? i : 0)
-                }),
-                other: state.other,
-                cases: state.cases * ncr(search.num, i),
-                failRate: state.failRate.map(function (v, ix) {
-                    return v * (ix == search.target ? Math.pow(search.failRate, i) : 1)
-                }),
-                handNum: state.handNum - i,
-                deckNum: state.deckNum - search.num,
-                targetsIx: state.targetsIx,
-                searchesIx: state.searchesIx + 1
-            });
-            hands = hands.concat(res.hands);
-            successRate += res.successRate;
-        }
-        return { hands: hands, successRate: successRate };
-    } else {
-        /* 「欲しいカード」とサーチを引く枚数が確定 → 残りは適当なカード */
-        return solveR({
-            targets: state.targets,
-            searches: state.searches,
-            other: state.other + state.handNum,
-            cases: state.cases * ncr(state.deckNum, state.handNum),
-            failRate: state.failRate,
-            handNum: 0,
-            deckNum: state.deckNum - state.handNum,
-            targetsIx: state.targetsIx,
-            searchesIx: state.searchesIx
-        });
+  if (state.handNum == 0) {
+    /* 手札を引き終わった → 確率計算して返す */
+    var caseProb = state.cases / ncr(deckNum, handNum);
+    var successRate = state.failRate.reduce(function (l, r) { return l * (1.0 - r); }, 1);
+    return {
+      hands: [{
+        targets: state.targets,
+        searches: state.searches,
+        other: state.other,
+        prob: caseProb * 100
+      }],
+      successRate: caseProb * successRate * 100  /* この手札になる確率 x サーチの成功率 */
+    };
+  } else if (state.targetsIx < targets.length) {
+    /* 「欲しいカード」を各何枚素引きするかの場合分け */
+    var hands = [];
+    var successRate = 0;
+    var targetNum = targets[state.targetsIx];
+    for (var i = 0; i <= Math.min(targetNum, state.handNum); i++) {
+      /* targetsIx 番目の「欲しいカード」を i 枚素引きする場合 */
+      var res = solveR({
+        targets: state.targets.map(function (v, ix) {
+          return v + (ix == state.targetsIx ? i : 0)
+        }),
+        searches: state.searches,
+        other: state.other,
+        cases: state.cases * ncr(targetNum, i),
+        failRate: state.failRate.map(function (v, ix) {
+          return v * (ix == state.targetsIx ? Math.pow(0, i) : 1)
+        }),
+        handNum: state.handNum - i,
+        deckNum: state.deckNum - targetNum,
+        targetsIx: state.targetsIx + 1,
+        searchesIx: state.searchesIx
+      });
+      hands = hands.concat(res.hands);
+      successRate += res.successRate;
     }
+    return { hands: hands, successRate: successRate };
+  } else if (state.searchesIx < searches.length) {
+    /* サーチを各何枚引くかの場合分け */
+    var hands = [];
+    var successRate = 0;
+    var search = searches[state.searchesIx];
+    for (var i = 0; i <= Math.min(search.num, state.handNum); i++) {
+      /* searchesIx 番目のサーチを i 枚素引きする場合 */
+      var res = solveR({
+        targets: state.targets,
+        searches: state.searches.map(function (v, ix) {
+          return v + (ix == state.searchesIx ? i : 0)
+        }),
+        other: state.other,
+        cases: state.cases * ncr(search.num, i),
+        failRate: state.failRate.map(function (v, ix) {
+          return v * (ix == search.target ? Math.pow(search.failRate, i) : 1)
+        }),
+        handNum: state.handNum - i,
+        deckNum: state.deckNum - search.num,
+        targetsIx: state.targetsIx,
+        searchesIx: state.searchesIx + 1
+      });
+      hands = hands.concat(res.hands);
+      successRate += res.successRate;
+    }
+    return { hands: hands, successRate: successRate };
+  } else {
+    /* 「欲しいカード」とサーチを引く枚数が確定 → 残りは適当なカード */
+    return solveR({
+      targets: state.targets,
+      searches: state.searches,
+      other: state.other + state.handNum,
+      cases: state.cases * ncr(state.deckNum, state.handNum),
+      failRate: state.failRate,
+      handNum: 0,
+      deckNum: state.deckNum - state.handNum,
+      targetsIx: state.targetsIx,
+      searchesIx: state.searchesIx
+    });
+  }
 }
 
 /* 階乗 */
 var facts = [1];
 function fact (n) {
-    if (!facts[n]) {
-        facts[n] = fact(n - 1) * n;
-    }
-    return facts[n];
+  if (!facts[n]) {
+    facts[n] = fact(n - 1) * n;
+  }
+  return facts[n];
 }
 
 /* n 個から r 個選ぶ組み合わせ */
 function ncr (n, r) {
-    return fact(n) / fact(n - r) / fact(r);
+  return fact(n) / fact(n - r) / fact(r);
 }
